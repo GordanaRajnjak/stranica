5ff0657c639e09a04206eb8f317f6d8262052e9c
GordanaRajnjak <gogashkk@gmail.com>
2017-05-23 09:52:44 +0200

    First commit

---
+++ js/Contact.js
@@ -0,0 +1,32 @@
+
+
+
+
+/*
+function initMap() {
+        var uluru = {lat: {45.250620, lng: 19.838374};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+*/
+function myMap() {
+  var myCenter = new google.maps.LatLng(45.250620,19.838374);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+    
+
+google.maps.event.addDomListener(window, "resize", function() {
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center); 
+}); 
+    }
